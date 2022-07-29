be7078283c0842893c85f25206004989ab928d08
Robin Lee <sl5035@columbia.edu>
2022-07-29 21:18:21 +0900

    Chapter 7

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const path = require("path");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
 const BlogPost = require("./models/BlogPost.js");
 
 mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
 
 const app = new express();
+
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(fileUpload());
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
@@ -36,17 +41,24 @@ app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.get("/posts", (req, res) => {
-  // res.sendFile(path.resolve(__dirname,'pages/post.html'))
-  res.render("post");
+app.get("/post/:id", async (req, res) => {
+  const blogpost = await BlogPost.findById(req.params.id);
+
+  res.render("post", {
+    blogpost,
+  });
 });
 
 app.get("/posts/new", (req, res) => {
   res.render("create");
 });
 
-app.post("/posts/store", async (req, res) => {
+app.post("/posts/store", (req, res) => {
   // model creates a new doc with browser data
-  await BlogPost.create(req.body);
-  res.redirect("/");
+  let image = req.files.image;
+
+  image.mv(path.resolve(__dirname, "public/img", image.name), async (error) => {
+    await BlogPost.create({ ...req.body, image: "/img/" + image.name });
+    res.redirect("/");
+  });
 });
